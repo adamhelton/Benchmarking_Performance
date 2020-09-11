37e57e1e0152739e4d390b7ec6eedcacd244e054
Adam Helton <helton93@protonmail.com>
2020-09-11 14:35:26 -0400

    Initial commit

---
+++ IT481_AdamHelton_Unit7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IT481_AdamHelton_Unit7
+{
+    class Program
+    {
+        private static Stopwatch Stopwatch;
+        private static bool debug = false;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
